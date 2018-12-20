8dd0827b35ee34e5c908e07e5ce118822bfa49a6
ElZobaidy, Loay <Loay.El_zobaidy@emc.com>
2018-12-20 16:40:14 +0200

    Feature 162190801 (#10)
    * user signup for each cycle
    
    * signup

--- cycles/models.py
+++ cycles/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+import uuid
+from users.models import Skill,Employee
 
-from users.models import Skill
 # Create your models here.
 class Cycle(models.Model):
+    cycle_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=30,null=True)
     start_date = models.DateTimeField(auto_now=False, auto_now_add=False)
     end_date = models.DateTimeField(auto_now=False, auto_now_add=False)
     deadline = models.DateTimeField(auto_now=False, auto_now_add=False)
-    skills = models.ManyToManyField(Skill, related_name='cycles')
+    creation_date = models.DateTimeField(auto_now_add=True)
+    skills = models.ManyToManyField(Skill, related_name='cycles',default=None,blank=True)
+    mentors =  models.ManyToManyField(Employee, related_name='mentors')
+    mentees =  models.ManyToManyField(Employee, related_name='mentees')
+    def __str__(self):
+        return self.name
