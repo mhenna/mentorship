5451c93fe1c03524b4d7703a7cb8dfca90199f9d
WaelHassan, Menna <Menna.Wael_hassan@emc.com>
2019-01-27 17:04:52 +0200

    hours added for mentor (#21)

--- users/models.py
+++ users/models.py
@@ -22,6 +22,7 @@ class Employee(models.Model):
     matched = models.ManyToManyField("self",related_name='matches',blank=True)    
     skills = models.ManyToManyField(Skill, related_name='employees',blank=True)
     cycles = models.ManyToManyField(Cycle, related_name='employee',blank=True)
+    hours =  models.IntegerField(null=True)
    
     def __str__(self):
         return self.email+" -- "+str(self.id)
