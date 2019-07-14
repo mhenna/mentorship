fe2d33243b6a068b96558a4caf966eef9a2ff0a0
Mostafa Henna <mostafa.henna@dell.com>
2019-07-14 17:55:12 +0100

    reverted change in user serializer as it is out of the scope of the current branch

--- users/serializers.py
+++ users/serializers.py
@@ -9,7 +9,7 @@ class UserRetrieveSerializer(serializers.ModelSerializer):
     # answers = AnswerSerializer(many=True, read_only=True)
     class Meta:
         model = Employee
-        fields = ('id', 'email', 'first_name', 'last_name', 'years_of_experience', 'years_within_organization', 'is_mentor', 'departement', 'matched', 'cycles') 
+        fields = '__all__'
 
 class UserAnswerSerializer(serializers.ModelSerializer):
     class Meta:
